700847a43cda0e3273c47d3529f23d15cd144f55
john mukenge <johnsonmukenge@gmail.com>
2019-03-30 22:16:27 +0100

    My project setup completed successfully

---
+++ shoppingbackend/src/main/java/net/john/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.john.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
